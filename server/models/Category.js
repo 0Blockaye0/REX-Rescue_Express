d3c6abb9365169b5c56ba4ce89e7afe2901a8a65
Courtney Brown <clbrown0607@gmail.com>
2021-07-13 19:47:50 -0500

    updated Category.js and index.js

--- server/models/Category.js
+++ server/models/Category.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const categorySchema = new Schema({
-  name: {
+  size: {
     type: String,
     required: true,
     trim: true
