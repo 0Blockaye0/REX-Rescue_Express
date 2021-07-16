4e80bea59dab07fc982db30972cc921a116f53e6
Courtney Brown <clbrown0607@gmail.com>
2021-07-15 21:11:38 -0500

    updated dog and user seeds

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -14,138 +14,157 @@ db.once('open', async () => {
 
   console.log('categories seeded');
 
-  await Product.deleteMany();
+  await Dog.deleteMany();
 
-  const products = await Product.insertMany([
+  const dogs = await Dog.insertMany([
     {
-      name: 'Tin of Cookies',
+      name: 'Arrow',
+      description:
+        'Arrow is a handsome young dog looking for his forever home! He is very active and would be best with an owner that is a marathon runner and can throw a ball really, really far.',
+      image: 'arrow.jpg',
+      breed: 'Belgian Malinois',
+      age: '2 years',
+      category: categories[3]._id,
+    },
+    {
+      name: 'Rusty',
+      description:
+        'Playful great with kids, extremely active, hypoallergenic.',
+      image: 'rusty.jpg',
+      breed: 'Scottish Terrier',
+      age: '5 years',
+      category: categories[1]._id,
+    },
+    {
+      name: 'Bentley',
+      description:
+        'Very active and loves to fetch. Loves to play and is very sweet.',
+      image: 'bentley.jpg',
+      breed: 'Boston Terrier',
+      age: '5 years',
+      category: categories[1]._id,
+    },
+    {
+      name: 'Bella',
+      description:
+        'Loves to lick faces and is protective of her food.',
+      image: 'bella.jpg',
+      breed: 'Boston Terrier',
+      age: '5 years',
+      category: categories[1]._id,
+    },
+    {
+      name: 'Brady',
+      description:
+        'Very curious and loves to explore. Perfect sidekick who loves adventure.',
+      image: 'brady.jpg',
+      breed: 'Boston Terrier',
+      age: '5 years',
+      category: categories[1]._id,
+    },
+    {
+      name: 'Kai',
+      description:
+        'Kai likes laying on cold tile floor and leaving tufts of hair in every nook and cranny.',
+      image: 'kai.jpg',
+      breed: 'Siberian Husky',
+      age: '12 years',
+      category: categories[2]._id,
+    },
+    {
+      name: 'Leo',
+      description:
+        'Leo will let you know when someone is at the door and loves  to burrow in the blankets for mid-day naps.',
+      image: 'leo.jpg',
+      breed: 'Beagle / Dachshund Mix',
+      age: '12 years',
+      category: categories[1]._id,
+    },
+    {
+      name: 'Chief',
+      description:
+        'Chief is the perfect companion! He enjoys lots of belly rubs and his favorite activity is swimming.',
+      image: 'chief.jpg',
+      breed: 'German Shepherd',
+      age: '8 years',
+      category: categories[3]._id,
+    },
+    {
+      name: 'Echo',
+      description:
+        'Echo lives up to her name! This girl loves to bark and would make a great guard dog.',
+      image: 'echo.jpg',
+      breed: 'German Shepherd',
+      age: '5 years',
+      category: categories[3]._id,
+    },
+    {
+      name: 'Camelot',
+      description:
+        'Camelot is the floofiest boy in Rescue Express! He loves ice cream and hiking.',
+      image: 'camelot.jpg',
+      breed: 'Samoyed',
+      age: '3 years',
+      category: categories[2]._id,
+    },
+    {
+      name: 'Roux',
       description:
         'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
-      image: 'cookie-tin.jpg',
-      category: categories[0]._id,
-      price: 2.99,
-      quantity: 500
-    },
-    {
-      name: 'Canned Coffee',
-      description:
-        'Praesent sed lacinia mauris. Nulla congue nibh magna, at feugiat nunc scelerisque quis. Donec iaculis rutrum vulputate. Suspendisse lectus sem, vulputate ac lectus sed, placerat consequat dui.',
-      image: 'canned-coffee.jpg',
-      category: categories[0]._id,
-      price: 1.99,
-      quantity: 500
-    },
-    {
-      name: 'Toilet Paper',
+      image: 'roux.jpg',
+      breed: 'Corgi',
+      age: '3 years',
       category: categories[1]._id,
-      description:
-        'Donec volutpat erat erat, sit amet gravida justo sodales in. Phasellus tempus euismod urna. Proin ultrices nisi ut ipsum congue, vitae porttitor libero suscipit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam lacinia a nisi non congue.',
-      image: 'toilet-paper.jpg',
-      price: 7.99,
-      quantity: 20
     },
     {
-      name: 'Handmade Soap',
-      category: categories[1]._id,
+      name: 'Finn',
       description:
-        'Praesent placerat, odio vel euismod venenatis, lectus arcu laoreet felis, et fringilla sapien turpis vestibulum nisl.',
-      image: 'soap.jpg',
-      price: 3.99,
-      quantity: 50
-    },
-    {
-      name: 'Set of Wooden Spoons',
-      category: categories[1]._id,
-      description:
-        'Vivamus ut turpis in purus pretium mollis. Donec turpis odio, semper vel interdum ut, vulputate at ex. Duis dignissim nisi vel tortor imperdiet finibus. Aenean aliquam sagittis rutrum.',
-      image: 'wooden-spoons.jpg',
-      price: 14.99,
-      quantity: 100
-    },
-    {
-      name: 'Camera',
+        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+      image: 'finn.jpg',
+      breed: 'Mixed Breed',
+      age: '6 years',
       category: categories[2]._id,
-      description:
-        'Vestibulum risus metus, luctus non tortor quis, tincidunt consectetur ex. Nullam vitae lobortis ligula, ut sagittis massa. Curabitur consectetur, tellus at pulvinar venenatis, erat augue cursus erat, eu ullamcorper eros lectus ultrices ipsum. Integer rutrum, augue vitae auctor venenatis, turpis turpis elementum orci, at sagittis risus mi a leo.',
-      image: 'camera.jpg',
-      price: 399.99,
-      quantity: 30
     },
     {
-      name: 'Tablet',
+      name: 'Bucky',
+      description:
+        'Bucky is the quintessential Golden Retriever! He loves everyone and everything, especially water and birds! ',
+      image: 'bucky.jpg',
+      breed: 'Golden Retriever',
+      age: '3 years',
       category: categories[2]._id,
-      description:
-        'In sodales, ipsum quis ultricies porttitor, tellus urna aliquam arcu, eget venenatis purus ligula ut nisi. Fusce ut felis dolor. Mauris justo ante, aliquet non tempus in, tempus ac lorem. Aliquam lacinia dolor eu sem eleifend ultrices. Etiam mattis metus metus. Sed ligula dui, placerat non turpis vitae, suscipit volutpat elit. Phasellus sagittis, diam elementum suscipit fringilla, libero mauris scelerisque ex, ac interdum diam erat non sapien.',
-      image: 'tablet.jpg',
-      price: 199.99,
-      quantity: 30
     },
     {
-      name: 'Tales at Bedtime',
-      category: categories[3]._id,
+      name: 'Roshi',
       description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ornare diam quis eleifend rutrum. Aliquam nulla est, volutpat non enim nec, pharetra gravida augue. Donec vitae dictum neque. Pellentesque arcu lorem, fringilla non ligula ac, tristique bibendum erat. Ut a semper nibh. Quisque a mi et mi tempor ultricies. Maecenas eu ipsum eu enim hendrerit accumsan at euismod urna.',
-      image: 'bedtime-book.jpg',
-      price: 9.99,
-      quantity: 100
-    },
-    {
-      name: 'Spinning Top',
-      category: categories[4]._id,
-      description: 'Ut vulputate hendrerit nibh, a placerat elit cursus interdum.',
-      image: 'spinning-top.jpg',
-      price: 1.99,
-      quantity: 1000
-    },
-    {
-      name: 'Set of Plastic Horses',
-      category: categories[4]._id,
-      description:
-        'Sed a mauris condimentum, elementum enim in, rhoncus dui. Phasellus lobortis leo odio, sit amet pharetra turpis porta quis.',
-      image: 'plastic-horses.jpg',
-      price: 2.99,
-      quantity: 1000
-    },
-    {
-      name: 'Teddy Bear',
-      category: categories[4]._id,
-      description:
-        'Vestibulum et erat finibus erat suscipit vulputate sed vitae dui. Ut laoreet tellus sit amet justo bibendum ultrices. Donec vitae felis vestibulum, congue augue eu, finibus turpis.',
-      image: 'teddy-bear.jpg',
-      price: 7.99,
-      quantity: 100
-    },
-    {
-      name: 'Alphabet Blocks',
-      category: categories[4]._id,
-      description:
-        'Morbi consectetur viverra urna, eu fringilla turpis faucibus sit amet. Suspendisse potenti. Donec at dui ac sapien eleifend hendrerit vel sit amet lectus.',
-      image: 'alphabet-blocks.jpg',
-      price: 9.99,
-      quantity: 600
+        'Bad dog, but a good boy.',
+      image: 'roshi.jpg',
+      breed: 'Chesapeake Bay Retriever Mix',
+      age: '5 years',
+      category: categories[2]._id,
     }
   ]);
 
-  console.log('products seeded');
+  console.log('dogs seeded');
 
   await User.deleteMany();
 
   await User.create({
-    firstName: 'Pamela',
-    lastName: 'Washington',
-    email: 'pamela@testmail.com',
+    firstName: 'Jane',
+    lastName: 'Smith',
+    email: 'jane@email.com',
     password: 'password12345',
-    orders: [
+    applications: [
       {
-        products: [products[0]._id, products[0]._id, products[1]._id]
+        dogs: [dogs[0]._id]
       }
     ]
   });
 
   await User.create({
-    firstName: 'Elijah',
-    lastName: 'Holt',
-    email: 'eholt@testmail.com',
+    firstName: 'John',
+    lastName: 'Smith',
+    email: 'john@email.com',
     password: 'password12345'
   });
 
